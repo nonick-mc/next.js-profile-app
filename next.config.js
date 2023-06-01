4fb8b201b157faf64ace2da302c5b48cde5127a0
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2023-06-02 03:23:45 +0900

    fix: <Image>タグの画像が表示されない問題

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
   basePath: process.env.GITHUB_ACTIONS && "/nonick-mc.github.io",
   output: 'export',
   trailingSlash: true,
