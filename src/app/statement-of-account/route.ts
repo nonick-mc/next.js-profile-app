38c32c0eebe271d7c45ca4ee7dfb54413a56637b
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2023-10-11 18:14:48 +0900

    remove: /statement-of-account

--- src/app/statement-of-account/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export function GET() {
-  return NextResponse.redirect(
-    'https://drive.google.com/drive/folders/14dToRjyGnbVwU7eqHh11lqFMHTYztyYB?usp=sharing',
-  );
-}
